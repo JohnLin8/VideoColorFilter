972411df8ecfab8916bef11d0eec64087ec0982d
JohnLin8 <120913810@qq.com>
2021-12-30 18:01:37 +0800

    test camera color

--- app/src/main/java/com/test/videocolorfilter/MainActivity.java
+++ app/src/main/java/com/test/videocolorfilter/MainActivity.java
@@ -1,12 +1,17 @@
 package com.test.videocolorfilter;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.graphics.Canvas;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.media.MediaPlayer;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.os.Parcel;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -21,10 +26,14 @@ import android.widget.Toast;
 import com.test.videocolorfilter.camera.CameraHelper;
 import com.test.videocolorfilter.camera.CameraListener;
 
-public class MainActivity extends AppCompatActivity implements ViewTreeObserver.OnGlobalLayoutListener {
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+//implements ViewTreeObserver.OnGlobalLayoutListener
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity----";
-//    private GLSurfaceView mVideoView = null;
+    private GLSurfaceView mVideoView = null;
     private TextureView previewView = null;
 
     private ColorFilterMatrixUtil mColorFilterMatrixUtil = new ColorFilterMatrixUtil();
@@ -47,75 +56,85 @@ public class MainActivity extends AppCompatActivity implements ViewTreeObserver.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mVideoView = findViewById(R.id.video_view);
+        mVideoView = findViewById(R.id.video_view);
         previewView = findViewById(R.id.textureView);
-//        previewView.bringToFront();
+        previewView.bringToFront();
 
         //在布局结束后才做初始化操作
-        previewView.getViewTreeObserver().addOnGlobalLayoutListener(this);
+//        previewView.getViewTreeObserver().addOnGlobalLayoutListener(this);
 //        init();
+        initData();
+        setVideoView();
+        initCamera();
     }
 
-//    private void initData() {
-//        //设置版本2
-//        mVideoView.setEGLContextClientVersion(2);
-//        //设置渲染器
-//        mVideoView.setRenderer(new GLSurfaceView.Renderer() {
-//            @Override
-//            public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-//                mReactShape = new RectShape();
-//                int textureId = GLUtil.generateOESTexture();
-//                mSurfaceTexture = new SurfaceTexture(textureId);
-//                mSurfaceTexture.setOnFrameAvailableListener(new SurfaceTexture.OnFrameAvailableListener() {
-//                    @Override
-//                    public void onFrameAvailable(SurfaceTexture surfaceTexture) {
-//                        mFrameAvailable = true;
-//                    }
-//                });
-//                mSurface = new Surface(mSurfaceTexture);
-//                mReactShape.setTextureId(textureId);
-////                initCamera();
-////                setupPlayer();
-//            }
-//
-//            @Override
-//            public void onSurfaceChanged(GL10 gl10, int i, int i1) {
-//
-//            }
-//
-//            @Override
-//            public void onDrawFrame(GL10 gl10) {
-//                if (mFrameAvailable) {
-//                    mSurfaceTexture.updateTexImage();
-//                    mFrameAvailable = false;
-//                }
-//
-//                float[] colorFilter = mColorFilterMatrixUtil.getColorFilterArray16();
-//                mReactShape.setColorFilterArray(colorFilter);
-//                mReactShape.draw();
-//            }
-//        });
-//    }
-//
-//    private void setupPlayer() {
-//        try {
-//            mMediaPlayer = MediaPlayer.create(this, R.raw.testfile);
-//            mMediaPlayer.setSurface(mSurface);
-//            mMediaPlayer.setLooping(true);
-//            mMediaPlayer.start();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public void setVideoView() {
-//        mHueValue = -15;        //-180~180
-//        mSaturationValue = 1;    //-1~1
-//        mLightnessValue = 1;     //-1~1
-//        mColorFilterMatrixUtil.setHue(mHueValue);
-//        mColorFilterMatrixUtil.setSaturation(mSaturationValue);
-//        mColorFilterMatrixUtil.setLightness(mLightnessValue);
-//    }
+    private void initData() {
+        //设置版本2
+        mVideoView.setEGLContextClientVersion(2);
+        //设置渲染器
+        mVideoView.setRenderer(new GLSurfaceView.Renderer() {
+            @Override
+            public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+                mReactShape = new RectShape();
+                int textureId = GLUtil.generateOESTexture();
+                mSurfaceTexture = new SurfaceTexture(textureId);
+                mSurfaceTexture.setOnFrameAvailableListener(new SurfaceTexture.OnFrameAvailableListener() {
+                    @Override
+                    public void onFrameAvailable(SurfaceTexture surfaceTexture) {
+                        mFrameAvailable = true;
+                    }
+                });
+                mSurface = new Surface(mSurfaceTexture);
+                mReactShape.setTextureId(textureId);
+
+                Rect rect = new Rect();
+                Canvas canvas = mSurface.lockCanvas(rect);
+
+//                previewView.setSurfaceTexture(mSurfaceTexture);
+//                initCamera();
+//                setupPlayer();
+
+            }
+
+            @Override
+            public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+
+            }
+
+            @Override
+            public void onDrawFrame(GL10 gl10) {
+                if (mFrameAvailable) {
+                    mSurfaceTexture.updateTexImage();
+                    mFrameAvailable = false;
+                }
+
+                float[] colorFilter = mColorFilterMatrixUtil.getColorFilterArray16();
+                mReactShape.setColorFilterArray(colorFilter);
+                mReactShape.draw();
+            }
+        });
+    }
+
+    private void setupPlayer() {
+        try {
+            mMediaPlayer = MediaPlayer.create(this, R.raw.testfile);
+            mMediaPlayer.setSurface(mSurface);
+            mMediaPlayer.setLooping(true);
+            mMediaPlayer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //
+    public void setVideoView() {
+        mHueValue = -15;        //-180~180
+        mSaturationValue = 1;    //-1~1
+        mLightnessValue = 1;     //-1~1
+        mColorFilterMatrixUtil.setHue(mHueValue);
+        mColorFilterMatrixUtil.setSaturation(mSaturationValue);
+        mColorFilterMatrixUtil.setLightness(mLightnessValue);
+    }
 
     private void initCamera() {
         DisplayMetrics metrics = new DisplayMetrics();
@@ -131,7 +150,12 @@ public class MainActivity extends AppCompatActivity implements ViewTreeObserver.
 
             @Override
             public void onPreview(final byte[] nv21, Camera camera) {
-
+                int[] rgb = new int[400 * 240];
+                decodeYUV420SP(rgb, nv21, 400, 240);
+                @SuppressLint("Recycle") Parcel parcel = Parcel.obtain();
+//                parcel.writeByteArray(nv21);
+                parcel.writeIntArray(rgb);
+                mSurface.readFromParcel(parcel);
             }
 
             @Override
@@ -154,15 +178,16 @@ public class MainActivity extends AppCompatActivity implements ViewTreeObserver.
         cameraHelper = new CameraHelper.Builder()
                 .previewViewSize(new Point(previewView.getMeasuredWidth(), previewView.getMeasuredHeight()))
                 .rotation(getWindowManager().getDefaultDisplay().getRotation())
-                .additionalRotation(90)
-                .specificCameraId(1)
-                .isMirror(false)
+                .additionalRotation(180)
+                .specificCameraId(0)
+                .isMirror(true)
                 .previewOn(previewView)
                 .cameraListener(cameraListener)
                 .build();
         cameraHelper.init();
         cameraHelper.start();
     }
+
 
     /**
      * 所需的所有权限信息
@@ -219,14 +244,105 @@ public class MainActivity extends AppCompatActivity implements ViewTreeObserver.
 //                initData();
 //                setVideoView();
             } else {
-                Toast.makeText(this,"no permission",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "no permission", Toast.LENGTH_LONG).show();
             }
         }
     }
 
-    @Override
-    public void onGlobalLayout() {
-        previewView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-        init();
+//    @Override
+//    public void onGlobalLayout() {
+//        previewView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+//        init();
+//    }
+
+    static public void decodeYUV420SP(int[] rgb, byte[] yuv420sp, int width, int height) {
+        final int frameSize = width * height;
+
+        for (int j = 0, yp = 0; j < height; j++) {
+            int uvp = frameSize + (j >> 1) * width, u = 0, v = 0;
+            for (int i = 0; i < width; i++, yp++) {
+                int y = (0xff & ((int) yuv420sp[yp])) - 16;
+                if (y < 0)
+                    y = 0;
+                if ((i & 1) == 0) {
+                    v = (0xff & yuv420sp[uvp++]) - 128;
+                    u = (0xff & yuv420sp[uvp++]) - 128;
+                }
+
+                int y1192 = 1192 * y;
+                int r = (y1192 + 1634 * v);
+                int g = (y1192 - 833 * v - 400 * u);
+                int b = (y1192 + 2066 * u);
+
+                if (r < 0)
+                    r = 0;
+                else if (r > 262143)
+                    r = 262143;
+                if (g < 0)
+                    g = 0;
+                else if (g > 262143)
+                    g = 262143;
+                if (b < 0)
+                    b = 0;
+                else if (b > 262143)
+                    b = 262143;
+
+                rgb[yp] = 0xff000000 | ((r << 6) & 0xff0000) | ((g >> 2) & 0xff00) | ((b >> 10) & 0xff);
+            }
+        }
+    }
+
+    public void decodeYUV(int[] out, byte[] fg, int width, int height) throws NullPointerException, IllegalArgumentException {
+
+        int sz = width * height;
+        if (out == null)
+            throw new NullPointerException("buffer out is null");
+        if (out.length < sz)
+            throw new IllegalArgumentException("buffer out size " + out.length + " < minimum " + sz);
+        if (fg == null)
+            throw new NullPointerException("buffer *fg* is null");
+        if (fg.length < sz)
+            throw new IllegalArgumentException("buffer fg size " + fg.length + " < minimum " + sz * 3 / 2);
+        int i, j;
+        int Y, Cr = 0, Cb = 0;
+        for (j = 0; j < height; j++) {
+            int pixPtr = j * width;
+            final int jDiv2 = j >> 1;
+            for (i = 0; i < width; i++) {
+                Y = fg[pixPtr];
+                if (Y < 0)
+                    Y += 255;
+                if ((i & 0x1) != 1) {
+                    final int cOff = sz + jDiv2 * width + (i >> 1) * 2;
+                    Cb = fg[cOff];
+                    if (Cb < 0)
+                        Cb += 127;
+                    else
+                        Cb -= 128;
+                    Cr = fg[cOff + 1];
+                    if (Cr < 0)
+                        Cr += 127;
+                    else
+                        Cr -= 128;
+                }
+                int R = Y + Cr + (Cr >> 2) + (Cr >> 3) + (Cr >> 5);
+                if (R < 0)
+                    R = 0;
+                else if (R > 255)
+                    R = 255;
+                int G = Y - (Cb >> 2) + (Cb >> 4) + (Cb >> 5) - (Cr >> 1)
+                        + (Cr >> 3) + (Cr >> 4) + (Cr >> 5);
+                if (G < 0)
+                    G = 0;
+                else if (G > 255)
+                    G = 255;
+                int B = Y + Cb + (Cb >> 1) + (Cb >> 2) + (Cb >> 6);
+                if (B < 0)
+                    B = 0;
+                else if (B > 255)
+                    B = 255;
+                out[pixPtr++] = 0xff000000 + (B << 16) + (G << 8) + R;
+            }
+        }
     }
 }
